e957f454937c9782835f85c50733003c17388b15
Changmin Bae <baec23@gmail.com>
2023-01-12 18:27:46 +0900

    Date Picker WIP

--- app/src/main/java/com/baec23/composetemplate/ui/screen/samplescreen/SampleViewModel.kt
+++ app/src/main/java/com/baec23/composetemplate/ui/screen/samplescreen/SampleViewModel.kt
@@ -1,5 +1,8 @@
 package com.baec23.composetemplate.ui.screen.samplescreen
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
 import com.baec23.composetemplate.service.SnackbarService
@@ -11,13 +14,17 @@ class SampleViewModel @Inject constructor(
     private val navController: NavHostController,
     private val snackbarService: SnackbarService,
 ) : ViewModel() {
+    private val _showDatePicker: MutableState<Boolean> = mutableStateOf(false)
+    val showDatePicker: State<Boolean> = _showDatePicker
     fun onEvent(event: SampleUiEvent) {
         when (event) {
-            SampleUiEvent.SampleButtonPressed -> snackbarService.showSnackbar("Button pressed!")
+            SampleUiEvent.SampleButtonPressed -> _showDatePicker.value = true
+            SampleUiEvent.DatePickerDismissed -> _showDatePicker.value = false
         }
     }
 }
 
 sealed class SampleUiEvent {
     object SampleButtonPressed : SampleUiEvent()
+    object DatePickerDismissed : SampleUiEvent()
 }
